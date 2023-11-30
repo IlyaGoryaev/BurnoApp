571e904b562a8ed725db7a6728ff7b5e92f7de84
IlyaGoryaev <128461591+IlyaGoryaev@users.noreply.github.com>
2023-11-30 09:37:39 +0300

    Initial Commit

---
+++ BurnoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BurnoApp
+//
+//  Created by Илья Горяев on 30.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
